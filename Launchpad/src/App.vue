a3da7cbd14d7d9416f470e09399e3c46ce859181
Dystorr <79255127+Dystorr@users.noreply.github.com>
2022-02-08 10:15:29 +0100

    Upload

---
+++ Launchpad/src/App.vue
@@ -0,0 +1,28 @@
+
+<template>
+
+  <!--<router-view />-->
+  <ToolBar/>
+  <Launchpad/>
+</template>
+
+<style lang="scss">
+  @import "./css/app.scss";
+</style>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ToolBar from "./components/ToolBar.vue";
+import ToolBox from "./components/ToolBox.vue";
+import Launchpad from "./components/Launchpad.vue";
+
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ToolBar,
+    ToolBox,
+    Launchpad
+  }
+})
+</script>
---
+++ Launchpad/src/components/Launchpad.vue
@@ -0,0 +1,73 @@
+
+<template>
+  <div class="q-pa-md">
+    <q-table
+      grid
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+      row-width="100px"
+      hide-bottom
+      id="LaunchpadWrapper"
+
+    >
+    </q-table>
+  </div>
+</template>
+
+<style lang="scss">
+  @import "../css/app.scss";
+</style>
+
+
+<script>
+
+const columns = [
+  {
+    name: 'desc',
+    required: true,
+    label: 'Dessert (100g serving)',
+    align: 'left',
+    field: row => row.name,
+    sortable: true
+  },
+]
+
+const rows = [
+  {
+    name: 'Frozen Yogurt',
+    calories: 159,
+    fat: 6.0,
+    carbs: 24
+  },
+  {
+    name: 'Ice cream sandwich',
+    calories: 237,
+    fat: 9.0,
+    carbs: 37
+  },
+  {
+    name: 'Eclair',
+    calories: 262,
+    fat: 16.0,
+    carbs: 23
+  },
+  {
+    name: 'Cupcake',
+    calories: 305,
+    fat: 3.7,
+    carbs: 67
+  },
+
+]
+
+export default {
+    name: "Launchpad",
+  setup () {
+    return {
+      columns,
+      rows
+    }
+  }
+}
+</script>
